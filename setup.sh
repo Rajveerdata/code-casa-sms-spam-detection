e5fa1aad7f9a166e03c2de9fcbf2608b6eaf30e9
Rajveer Singh Rathore <127951177+Rajveerdata@users.noreply.github.com>
2023-09-17 13:05:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 
-mkdir -p ~/.streamlit/
+#!/bin/bash
 
+# Create a .streamlit directory in the root of your project
+mkdir -p .streamlit
+
+# Create a config.toml file inside the .streamlit directory
 echo "\
 [server]\n\
 port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
-" > ~/.streamlit/config.toml
+" > .streamlit/config.toml
